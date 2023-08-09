a2e484a97759c9a2ec716869cc9486c6ca1fda40
wesle <wesleyorrr@gmail.com>
2023-08-09 11:22:32 -0300

    todobinding

--- app/src/main/java/com/curvelo/todolist/MainActivity.kt
+++ app/src/main/java/com/curvelo/todolist/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.curvelo.todolist.ui.theme.TodoListTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
